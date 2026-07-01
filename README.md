eadebd08990f8f38a452294d21d83d24378703ae
Zeo <67521686+ThatZeoGal@users.noreply.github.com>
2026-06-30 19:07:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# TempoDough
+A wavmark (wmb) creator for Rhythm Heaven Groove.
+
+## What's a wavmark?
+Wavmark is a small format used to store tempo data. It is made up of a series of "section markers", each of which holds the sample position of the marker and the amount of ticks between the marker and the next one (480 ticks = 1 beat).
+
+The file also controls where the level chart begins playback. When the chosed section marker is reached, that's when chart playback begins. This can be used to start running game events later than the immediate start of the audio file.
+
+Finally, there are some other values in the file. The first 4 bytes are always set to 0, as they are written to when loaded into memory. There is a field that likely corresponds to he indx of the sectio where looping starts, and there is also a field (either 0 or -1) that has no clear effect currently.
+
+## Building
+This project uses the Ninja build system. Simply run `ninja` in the project directory to build.
+
+## Credits
+- Wavmark research by [Zeo](https://github.com/ThatZeoGal), [Patata](https://github.com/patataofcourse), and [Conhlee](https://github.com/conhlee)
+- Tool created by [Zeo](https://github.com/ThatZeoGal), with massive help from [Conhlee](https://github.com/conhlee)
---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+#include <Macro.hpp>
+#include <File.hpp>
+#include <Error.hpp>
+
+struct TempoChange
+{
+    f32 beat;
+    f32 bpm;
+};
+
+struct WavMarkSectionMarker
+{
+    u32 posSample;
+    u32 lenTick;
+};
+
+struct WavMarkData
+{
+    u32 reserved;
+    u32 sampleRate;
+    u32 unk08;
+    u32 chartStartIndex;
+    u32 loopStartIndex;
+};
+
+int main(int argc, char **argv)
+{
+    if (argc < 5)
+    {
+        printf("TempoDough v1.0\n");
+        printf("TempoDough was built " __DATE__ " " __TIME__ "\n\n");
+
+        printf("usage: %s <path to wmb> <chart start index> <song length in beats> <beat,bpm>...\n", argv[0]);
+        printf("          <song length in beats> (float): Beats start at 0.\n");
+        printf("          <chart start index> (int): The chart will start when this tempo change is reached.\n");
+        printf("                                     This is zero-indexed.\n");
+        printf("          <beat,bpm> (float, float): The beat of the tempo change, and the bpm to change to.\n");
+        printf("                                     At least 1 pair is required. The first pair must be at beat 0.\n");
+        printf("                                     Add more pairs to have more tempo sections.\n");
+        printf("                                     Beats start at 0.\n");
+        return 1;
+    }
+
+    // get args
+    const char *pathToWMB = argv[1];
+    f32 songLengthBeats = strtof(argv[2], NULL);
+    s32 chartStartIndex = strtol(argv[3], NULL, 0);
+
+    u32 tempoChangeCount = argc - 4;
+    TempoChange *tempoChange = new TempoChange[tempoChangeCount];
+
+    for (u32 i = 4; i < argc; i++)
+    {
+        const char *seqStart = argv[i];
+        const char *commaPos = strchr(seqStart, ',');
+        if (commaPos == NULL)
+        {
+            Panic("Beat/bpm pairs need a comma!");
+        }
+        tempoChange[i - 4].beat = strtof(seqStart, NULL);
+        tempoChange[i - 4].bpm = strtof(commaPos + 1, NULL);
+    }
+
+    // make file
+    FILE *fp = fopen(pathToWMB, "wb");
+    if (fp == NULL)
+    {
+        Panic("Invalid file path.");
+    }
+
+    WavMarkData header;
+    header.reserved = 0;
+    header.sampleRate = 48000;
+    header.unk08 = 0;
+    header.chartStartIndex = chartStartIndex;
+    header.loopStartIndex = 0;
+
+    fwrite(&header, sizeof(WavMarkData), 1, fp);
+
+    WavMarkSectionMarker entry;
+    u32 nextPosSample = 0;
+    for (u32 i = 0; i < tempoChangeCount; i++)
+    {
+        f32 sectionLengthBeats = 0;
+
+        if (i + 1 < tempoChangeCount)
+        {
+            sectionLengthBeats = tempoChange[i + 1].beat - tempoChange[i].beat;
+        }
+        else
+        {
+            sectionLengthBeats = songLengthBeats - tempoChange[i].beat;
+        }
+
+        f32 sampleCount = (60.0f / tempoChange[i].bpm) * header.sampleRate * sectionLengthBeats;
+        f32 ticks = ((tempoChange[i].bpm * sampleCount) / 60.0f / header.sampleRate) * 480.0f;
+
+        entry.posSample = nextPosSample;
+        entry.lenTick = ticks;
+
+        nextPosSample += sampleCount;
+
+        fwrite(&entry, sizeof(WavMarkSectionMarker), 1, fp);
+    }
+
+    entry.posSample = nextPosSample;
+    entry.lenTick = 0;
+    fwrite(&entry, sizeof(WavMarkSectionMarker), 1, fp);
+
+    fclose(fp);
+
+    delete[] tempoChange;
+    return 0;
+}
